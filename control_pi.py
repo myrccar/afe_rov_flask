923744134571fe7723235475b19c86c95abdb4f8
myrccar <4myrccar@gmail.com>
2023-01-28 09:18:30 -0800

    untested flask api

--- control_pi.py
+++ control_pi.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import xbox
 
 """
 control_pi.py:
@@ -10,11 +11,25 @@ made by myrccar
 
 
 app = Flask(__name__)
-
+joy = xbox.JoyStick()
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def data():
+    controller_data = {
+        "axis-0":joy.leftX(),
+        "axis-1":joy.leftY(),
+        "axis-2":joy.leftTrigger(),
+        "axis-3":joy.rightX(),
+        "axis-4":joy.rightY(),
+        "axis-5":joy.rightTrigger(),
+        "button-1":joy.B(),
+        "button-2":joy.X(),
+        "button-3":joy.Y(),
+        "button-0":joy.A()
+    }
+
+    return controller_data
+
 
 if __name__ == '__main__':
     app.run()

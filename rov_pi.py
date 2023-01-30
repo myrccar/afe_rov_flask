546d54bd8cc3fc156b37816baf61728cd09899af
myrccar <4myrccar@gmail.com>
2023-01-29 16:19:56 -0800

    merdge thing

--- rov_pi.py
+++ rov_pi.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import RPi.GPIO as GPIO  
 
 """
 rov_pi.py:
@@ -7,7 +8,15 @@ gets controller data use the control_i.py api
 run motors(later)
 """
 
-while True:
-    r = requests.get("http://localhost:5000/")
-    data=r.json()
-    print(data)
+GPIO.setmode(GPIO.BOARD)             # choose BCM or ---BOARD(one we use)---  
+GPIO.setup(12, GPIO.OUT)
+
+try:
+    while True:
+        r = requests.get("http://192.168.0.98",verify=False)
+        data=r.json()
+        GPIO.output(12,data["button-0"])
+
+
+except KeyboardInterrupt:
+    GPIO.cleanup()

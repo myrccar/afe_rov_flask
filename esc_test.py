1792ab0b8226f2538560f9270b101fe9107258af
myrccar <4myrccar@gmail.com>
2023-03-15 11:17:31 -0700

    esc working

--- esc_test.py
+++ esc_test.py
@@ -1,25 +1,59 @@
-"""
-a script to test the esc/motors on the rov pi
-"""
-import RPi.GPIO as GPIO
+#!/usr/bin/env python
+
+# servo_demo.py
+# 2016-10-07
+# Public Domain
+
+# servo_demo.py          # Send servo pulses to GPIO 4.
+# servo_demo.py 23 24 25 # Send servo pulses to GPIO 23, 24, 25.
+
+import sys
 import time
+import random
+import pigpio
 
-# Set up GPIO
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(12, GPIO.OUT)
+NUM_GPIO=32
 
-# Set up PWM
-pwm = GPIO.PWM(12, 50)
-pwm.start(0)
+MIN_WIDTH=1000
+MAX_WIDTH=2000
 
-# Set speed to 50%
-pwm.ChangeDutyCycle(50)
-time.sleep(5)
+step = [0]*NUM_GPIO
+width = [0]*NUM_GPIO
+used = [False]*NUM_GPIO
 
-# Stop PWM
-pwm.stop()
+pi = pigpio.pi()
 
-# Clean up GPIO
-GPIO.cleanup()
+if not pi.connected:
+   exit()
 
+if len(sys.argv) == 1:
+   G = [4]
+else:
+   G = []
+   for a in sys.argv[1:]:
+      G.append(int(a))
+   
+
+print("Sending servos pulses to GPIO {}, control C to stop.".
+   format(' '.join(str(g) for g in G)))
+
+pi.set_servo_pulsewidth(G[0],1500)
+time.sleep(2.5)
+
+while True:
+
+   try:
+    
+    pi.set_servo_pulsewidth(G[0],1700)
+    time.sleep(0.1)
+    
+   except KeyboardInterrupt:
+      break
+
+print("\nTidying up")
+
+for g in G:
+   pi.set_servo_pulsewidth(g, 0)
+
+pi.stop()
 

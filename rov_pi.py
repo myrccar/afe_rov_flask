f1114978b0861a703408ab05bdd1e598cca0e957
myrccar <4myrccar@gmail.com>
2023-03-19 10:23:57 -0700

    upadted rov

--- rov_pi.py
+++ rov_pi.py
@@ -13,16 +13,16 @@ run motors(later)
 
 print("inisliseing ese 13,12,18,19 4s")
 pi.set_servo_pulsewidth(13,1500)
-# pi.set_servo_pulsewidth(12,1500)
-# pi.set_servo_pulsewidth(18,1500)
-# pi.set_servo_pulsewidth(19,1500)
+pi.set_servo_pulsewidth(12,1500)
+pi.set_servo_pulsewidth(18,1500)
+pi.set_servo_pulsewidth(19,1500)
 time.sleep(4)
 print("done")
 
 old_min = -1.0
 old_max = 1.0
-min_pwm = 1300
-max_pwm = 1700
+min_pwm = 1430
+max_pwm = 1600
 
 
 def convert_controller(old_value):
@@ -31,6 +31,13 @@ def convert_controller(old_value):
     new_value = ( (old_value - old_min) / (old_max - old_min) ) * (max_pwm - min_pwm) + min_pwm
     return int(new_value * 1)
 
+def tank_steering(horizontal, vertical):
+    left = convert_controller(vertical - horizontal)
+    right = convert_controller(vertical + horizontal)
+    print(horizontal,vertical)
+    return (left, right)
+
+
 
 try:
     while True:
@@ -38,6 +45,12 @@ try:
         data=r.json()
         #update escs
         pi.set_servo_pulsewidth(18,convert_controller(data['axis-4']))
+        pi.set_servo_pulsewidth(13,convert_controller(data['axis-3']))
+        left,right = tank_steering(data['axis-0'],data['axis-1'])
+        print(left,right)
+        pi.set_servo_pulsewidth(19,left)
+        pi.set_servo_pulsewidth(12,right)
+        
 
         
 
